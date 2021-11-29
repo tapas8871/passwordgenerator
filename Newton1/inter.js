4c6b8853a5b58865d4d02f98d91502a123b7db81
tapas <tapashnalge22@gmail.com>
2021-11-29 15:01:29 +0530

    firstcommit

---
+++ Newton1/inter.js
@@ -0,0 +1,62 @@
+// console.log(item);
+// var item = "cars";
+// let item2 = " car1";
+// const obj = {
+//     name : "tapas",
+//     print :console.log(this)
+// }
+
+//   var arr  = [ 2, 4, 6, 7]
+
+//   arr.forEach(e => ( console.log(e*2))
+   
+
+//   )
+
+
+
+    // <label for = "cars"> Choose a company:</label>
+    //   <select name =  "cars" id = "cars">
+    //     <option 
+    //     value = "volvo ">Volvo</option>
+    //     <option value = " eicher">Eicher</option>
+    //    <option  value ="tata">TATA</option></select>
+
+    //hoisting
+    var a = 10; //define
+    console.log(a);
+
+    // b = 20;
+    // console.log(b);
+    // let b;
+
+  //   function myfunc(){
+  //     console.log(a);
+  //   }
+
+  //   const myfunc2 = (x) =>{ //definaton part
+  //     console.log(x);
+  //   }
+  //   var x;
+  //   myfunc2(5); //function invokation part
+
+
+  //   // const obj ={
+  //   //   name : "tapas",
+
+  //   //   // show(){
+  //   //   //   return this.name
+  // //   //   // }
+  //   //   show = () =>{
+  //   //     return "hello";
+  //   //   }
+  //   // }
+
+  //  const  show = () =>{
+  //     return "hello";
+  //   }
+
+  //  let str = show();
+  //  console.log(str);
+
+  
